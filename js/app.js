1f37572e5faa5a49163d3ffd1e20db95b5989b82
Bsplay14 <brianstiven-14@hotmail.com>
2024-11-20 18:37:17 -0500

    corregir errores

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 //Importar metodos del api.js
-import { getPastel, getPastelbyID, updatePastel, deletePastel } from "./api";
+import { getPastel, getPastelbyID, updatePastel, deletePastel } from "./api.js";
 
 document.addEventListener("DOMContentLoaded", async() => {
     const pastelList = document.getElementById("pastel-list");
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", async() => {
     const pastel = await getPastel();
     pastelList.innerHTML = pastel.map(pastel => `
           <div class="col-xs-12 col-sm-6 col-md-3 card">
-            <div class="cars-body d-flex flex-column justify-content-end">
+            <div class="card-body d-flex flex-column justify-content-end">
               <h5 class="card-title">${pastel.name}</h5>
               <p class="card-text">${pastel.price}</p> 
               <a onclick="viewPastel(${pastel.id})" class="btn btn-primary">Ver mas</a>
@@ -19,32 +19,32 @@ document.addEventListener("DOMContentLoaded", async() => {
 //Metodo para ver los detalles del producto cuando damos clic en el boton "ver mas"
 window.viewPastel = async (id) => {
     const pastel = await getPastelbyID(id);
-
+    
     const pastelDetails = `
       <div class="col">
         <h3>${pastel.name}</h3>
+        <p>${pastel.description}</p>
         <p>${pastel.price}</p>
         <p>Precio: ${product.price}</p>
         <button class="btn btn-warning" onclick="enableEdit(${pastel.id})">Editar</button>
         <button class="btn btn-danger" onclick="deletePastel(${pastel.id})">Eliminar</button>
       </div>
     `;
-    document.getElementById("pastel-List").innerHTML = pastelDetails;
+    document.getElementById("pastel-list").innerHTML = pastelDetails;
 };
 
 //Vsita para editar la informacion
 window.enableEdit = async (id) => {
-const pastel = await getPastelbyID(id);
-
-const editForm = `
-  <div class="eow gap-3">
-    <imput type="text" id="name" value="${pastel.name}">
-    <textarea id="description">${pastel.description}</textarea>
-    input type="number" id="price" value=${pastel.price}">
-    <button class="btn btn-success" onclick="saveEdit(${id})">Guardar</button>
-  </div>  
-`;
-document.getElementById("pastel-list").innerHTML = editForm;
+  const pastel = await getPastelbyID(id);
+  const editForm = `
+    <div class="eow gap-3">
+      <imput type="text" id="name" value="${pastel.name}">
+      <textarea id="description">${pastel.description}</textarea>
+      <input type="number" id="price" value=${pastel.price}">
+      <button class="btn btn-success" onclick="saveEdit(${id})">Guardar</button>
+    </div>  
+    `;
+  document.getElementById("pastel-list").innerHTML = editForm;
 };
 
 //Guardar la informacion actualizada
